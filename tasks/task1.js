56a89c4b4a7c95a1020d3cb04c675127163b3aa2
frOstzz <luck123444@gmail.com>
2020-02-29 20:04:16 +0300

    fixed errors in solution

--- tasks/task1.js
+++ tasks/task1.js
@@ -2,12 +2,21 @@
 // Вывести через "-" все четные числа.
 
 const string = "1а2п3р4о5л6";
-const newArr = string.split("");
+const arr = string.split("");
 
-for (let i = 0; i < newArr.length; i += 1) {
-  if (!isNaN(+newArr[i])) {
-    newArr.splice(newArr[i], 1);
+for (let i = 0; i < arr.length; i += 1) {
+  if (!isNaN(+arr[i])) {
+    arr.splice(arr[i], 1);
   }
 }
+
+const newArr = [];
+
+for (let i = 0; i < arr.length; i += 1) {
+  if (+arr[i] % 2 === 0) {
+    newArr.push(arr[i]);
+  }
+}
+
 const newStr = newArr.join("-");
 console.log(newStr);
